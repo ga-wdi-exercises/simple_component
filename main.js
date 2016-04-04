4250a631dd82b104d6b6eda41462f2b454c964f9
Adam Bray <adam.bray@yale.edu>
2016-04-04 01:17:10 -0400

    converts to constructor function

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-var component = {
-  domElement: null,
-  initialize: function(container) {
+function Component() {
+  this.domElement = null;
+
+  this.initialize = function(container) {
     // create a dom element
     this.domElement = $('<div>');
     // attach it
     container.append(this.domElement);
-  },
-  render: function(statusText) {
+  };
+
+  this.render = function(statusText) {
     // update the dom element
     this.domElement.html(statusText);
-  }
-};
+  };
+}
 
-component.initialize($("#comp_demo"));
-component.render("This is a test!");
+var comp = new Component();
+comp.initialize($("#comp_demo"));
+comp.render("This is a test!");
 // component.render("This is a another test!");
